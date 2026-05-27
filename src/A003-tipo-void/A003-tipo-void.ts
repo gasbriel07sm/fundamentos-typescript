7e4b89c5258a171f119b7fe9fb6948e2f4dff18e
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-27 16:21:03 -0300

    Tipo void

---
+++ src/A003-tipo-void/A003-tipo-void.ts
@@ -0,0 +1,16 @@
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '));
+}
+
+const pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome);
+  }
+}
+
+pessoa.exibirNome();
+semRetorno('Olá', 'mundo!');
+
+export { pessoa };
