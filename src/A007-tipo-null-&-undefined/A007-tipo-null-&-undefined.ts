f387dd2dc7d38d5b4933df58b3acc929f5951d3e
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-27 16:52:39 -0300

    Tipo null & undefined

---
+++ src/A007-tipo-null-&-undefined/A007-tipo-null-&-undefined.ts
@@ -0,0 +1,20 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+function createPerson(firstName: string, lastName?: string): { firstName: string; lastName?: string } {
+  return { firstName, lastName };
+}
+
+function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwo = squareOf(2);
+
+if (squareOfTwo !== null) {
+  console.log(squareOfTwo.toFixed(2));
+} else {
+  console.log('Input is not a number');
+}
