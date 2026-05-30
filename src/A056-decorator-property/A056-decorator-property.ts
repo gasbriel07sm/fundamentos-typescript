ed8184514ae7b2a3db21a2f52317f190011ce073
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-30 12:21:37 -0300

    decorator-property

---
+++ src/A056-decorator-property/A056-decorator-property.ts
@@ -0,0 +1,41 @@
+function decorator(classPrototype: any, nome: string | symbol): any {
+  console.log(classPrototype);
+  console.log(nome);
+  let valorPropriedade: any;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      valorPropriedade = valor;
+    }
+  }
+}
+
+export class UmaPessoa {
+  @decorator
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
