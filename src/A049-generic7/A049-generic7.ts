32a82fb6b434f545091f9ea886d5bee6a633f127
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-30 08:50:11 -0300

    generics - 7

---
+++ src/A049-generic7/A049-generic7.ts
@@ -0,0 +1,19 @@
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
+
+console.log(isNumber('123'));
+console.log(isNumber(123));
+
+export function soma<T>(...args: T[]): number | null {
+  const retorno = args.reduce((sum, value) => {
+    if (isNumber(sum) && isNumber(value)) {
+      return sum + value;
+    }
+    return sum;
+  }, 0)
+
+  return retorno;
+}
+
+console.log(soma(1, 2, 3));
