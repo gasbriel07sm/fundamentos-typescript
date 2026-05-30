0573e5e9b8ca3ace670d91b4590859d1e0fcb9e2
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-30 09:26:50 -0300

    decoratos-class

---
+++ src/A051-decorator-class/A051-decorator-class.ts
@@ -0,0 +1,24 @@
+@decorator
+export class Animal {
+  constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    nome: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.cor = this.inverte(args[1]);
+      this.nome = this.inverte(args[0]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('Animal', 'roxo');
+console.log(animal);
